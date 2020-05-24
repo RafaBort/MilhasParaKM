585163bca9d72ff8144c8197ac37e0890f4e468c
RafaBort <rafaelafonseca.bort2019ri@gmail.com>
2020-05-24 19:00:19 -0300

    exercício resolvido

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MilhasParaKM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string milhas;
+     
+            Console.WriteLine("Coloque uma medida em milhas...");
+            milhas = Console.ReadLine();
+            Console.WriteLine("Milhas em KM vale 1,609");
+
+            Double KM1 = Convert.ToInt16(milhas);
+            Double y = KM1;
+            Double x = 1.609;
+
+            Double multiplicacao = ( x * KM1); 
+
+            Console.WriteLine(multiplicacao);
+
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
